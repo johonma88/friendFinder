99c0a6e95a0ef9151f62bb90791a8840b755e467
Johanna Honma <johonma@hotmail.com>
2018-02-13 22:36:48 -0700

    friendFinder

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friendsArray = [{
+        name: 'Snoopy',
+        photo: 'http://1.bp.blogspot.com/-A1RCZl4Fzzc/Ta9mfJhLifI/AAAAAAAAA-8/bMrBe4pQYDo/s1600/6684.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Garfield',
+        photo: 'http://4.bp.blogspot.com/-LrGVD-ICnJg/TfB27bEoz6I/AAAAAAAABf4/BsxfYLnyMjY/s1600/garfield_cartoon_wallpaper_1.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Cygnus',
+        photo: 'http://images4.fanpop.com/image/photos/22600000/Saint-Seiya-The-Knights-of-the-Zodiac-love-angels-22646017-1024-768.jpg',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Saylor Moon',
+        photo: 'http://images6.fanpop.com/image/photos/33900000/Sailor-Moon-sailor-moon-33978582-562-841.jpg',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Cloe',
+        photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Mrs. Piggy',
+        photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/Miss-Piggy-large-tonya-312.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Cookie Monster',
+        photo: 'http://www.digitaljournal.com/img/6/3/7/5/4/i/1/1/9/o/Cookiemonster2_FB.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    }
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Configure the Express application
+var app = express();
+var PORT = 8000;
+
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, (err) => {
+
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Friend Finder app is listening on PORT: ' + PORT);
+    }
+});
